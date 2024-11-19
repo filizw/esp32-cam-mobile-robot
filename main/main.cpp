0ce12290bb9cce545becd32f1716dadf9715515b
filizw <fizworski202@gmail.com>
2024-11-19 16:24:41 +0100

    Switch C++ standard to C++23

--- main/main.cpp
+++ main/main.cpp
@@ -1,12 +1,15 @@
 #include "driver/gpio.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "wifi.h"
 
 #define LED_PIN GPIO_NUM_4
 
 extern "C" void app_main(void)
 {
     gpio_set_direction(LED_PIN, GPIO_MODE_OUTPUT);
+
+    func();
 
     int level = 0;
 
