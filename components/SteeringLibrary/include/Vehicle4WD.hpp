e3c4186c08a10e34c7b421805e02d503eaa3dbfe
Toniqo <168441987+Toniqo@users.noreply.github.com>
2024-12-25 23:22:00 +0100

    updated steering library headers

--- components/SteeringLibrary/include/Vehicle4WD.hpp
+++ components/SteeringLibrary/include/Vehicle4WD.hpp
@@ -1,33 +1,200 @@
 #pragma once
 
-#include <iostream>
+#include <cmath>
+#include <vector>
+
+#include "driver/gpio.h"
+#include "driver/ledc.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_rom_gpio.h"
+#include "soc/gpio_sig_map.h"
+
+#define LED_PIN 2
+
+#define MOTOR_IN1_PIN 26
+#define MOTOR_IN2_PIN 25
+#define MOTOR_IN3_PIN 33
+#define MOTOR_IN4_PIN 32
+#define MOTOR_ENA_PIN 14
+#define MOTOR_ENB_PIN 27
+
+#define SERVO_STEERING_PIN 12
 
 class Vehicle4WD {
 private:
-    //0 == stop, 1 == forward, 2 == backward
-    int m_leftWheels;
+    int m_leftWheels;   //0 == stop, 1 == forward, 2 == backward
     int m_rightWheels;
 
+    int m_speed;    //working range 40 - 100
+
+    int m_pwmResolution;
+
 public:
-    Vehicle4WD(int left, int right) : m_leftWheels(left), m_rightWheels(right) {}
+    Vehicle4WD(int left = 0, int right = 0, int speed = 50) : m_leftWheels(left), m_rightWheels(right), m_speed(speed) {
+        gpio_set_direction((gpio_num_t)LED_PIN, GPIO_MODE_OUTPUT);
+        gpio_set_direction((gpio_num_t)MOTOR_IN1_PIN, GPIO_MODE_OUTPUT);
+        gpio_set_direction((gpio_num_t)MOTOR_IN2_PIN, GPIO_MODE_OUTPUT);
+        gpio_set_direction((gpio_num_t)MOTOR_IN3_PIN, GPIO_MODE_OUTPUT);
+        gpio_set_direction((gpio_num_t)MOTOR_IN4_PIN, GPIO_MODE_OUTPUT);
+        gpio_set_direction((gpio_num_t)MOTOR_ENA_PIN, GPIO_MODE_OUTPUT);
+        gpio_set_direction((gpio_num_t)MOTOR_ENB_PIN, GPIO_MODE_OUTPUT);
+        gpio_set_direction((gpio_num_t)SERVO_STEERING_PIN, GPIO_MODE_OUTPUT);
 
-    void driveForward() {
-        m_leftWheels = 1;
-        m_rightWheels = 1;
+        ledc_timer_config_t ledc_timer_0 = {};
+        ledc_timer_0.speed_mode = LEDC_LOW_SPEED_MODE;  // Tryb niskiej prędkości (wolniejsze czasy odpowiedzi)
+        ledc_timer_0.timer_num = LEDC_TIMER_0;         // Wybór jednego z dostępnych timerów (LEDC_TIMER_0 do LEDC_TIMER_3)
+        ledc_timer_0.duty_resolution = LEDC_TIMER_13_BIT; // Rozdzielczość PWM (2^13 = 8192 poziomów)
+        ledc_timer_0.freq_hz = 50;                   // Częstotliwość sygnału PWM w Hz
+        ledc_timer_0.clk_cfg = LEDC_AUTO_CLK;
+
+        ledc_channel_config_t ledc_channel_0 = {};
+        ledc_channel_0.speed_mode = LEDC_LOW_SPEED_MODE;
+        ledc_channel_0.channel = LEDC_CHANNEL_0;
+        ledc_channel_0.timer_sel = LEDC_TIMER_0;
+        ledc_channel_0.intr_type = LEDC_INTR_DISABLE;
+        ledc_channel_0.gpio_num = MOTOR_ENA_PIN;
+        ledc_channel_0.duty = 0;
+        ledc_channel_0.hpoint = 0;
+
+        ledc_channel_config_t ledc_channel_1 = {};
+        ledc_channel_1.speed_mode = LEDC_LOW_SPEED_MODE;
+        ledc_channel_1.channel = LEDC_CHANNEL_1;
+        ledc_channel_1.timer_sel = LEDC_TIMER_0;
+        ledc_channel_1.intr_type = LEDC_INTR_DISABLE;
+        ledc_channel_1.gpio_num = SERVO_STEERING_PIN;
+        ledc_channel_1.duty = 0;
+        ledc_channel_1.hpoint = 0;
+
+        m_pwmResolution = pow(2, ledc_timer_0.duty_resolution);
+
+        ledc_timer_config(&ledc_timer_0);
+        ledc_channel_config(&ledc_channel_0);
+        ledc_channel_config(&ledc_channel_1);
+
+        esp_rom_gpio_connect_out_signal(MOTOR_ENB_PIN, LEDC_LS_SIG_OUT0_IDX, false, false);
+
+        ledc_set_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_0, m_speed*m_pwmResolution/100);
+        ledc_update_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_0);
+        ledc_set_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_1, 615);
+        ledc_update_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_1);
     }
 
-    void driveBackward() {
-        m_leftWheels = 2;
-        m_rightWheels = 2;
-    }
-    //car1.turnLeft(angle/percentage)
-    virtual void turnLeft() {
-        m_leftWheels = 2;
-        m_rightWheels = 1;
+    void setSpeed(int speed) {
+        if (speed >= 0 && speed <= 100) {
+            m_speed = speed;
+            ledc_set_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_0, m_speed*m_pwmResolution/100);
+            ledc_update_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_0);
+        }
     }
 
-    virtual void turnRight() {
-        m_leftWheels = 1;
-        m_rightWheels = 2;
+    int getPwmResolution() const {
+        return m_pwmResolution;
+    }
+
+    int getSpeed() const {
+        return m_speed;
+    }
+
+    std::vector<int> getDirection() const {
+        std::vector<int> dirVector = {m_leftWheels, m_rightWheels};
+        return dirVector;
+    }
+
+    //car1.driveForward(duration) 0 == infinite drive
+    void driveForward(int duration) {
+        if (duration > 0) {
+            m_leftWheels = 1;
+            m_rightWheels = 1;
+            
+            gpio_set_level((gpio_num_t)MOTOR_IN1_PIN, 1);
+            gpio_set_level((gpio_num_t)MOTOR_IN2_PIN, 0);
+            gpio_set_level((gpio_num_t)MOTOR_IN3_PIN, 1);
+            gpio_set_level((gpio_num_t)MOTOR_IN4_PIN, 0);
+
+            vTaskDelay(duration/portTICK_PERIOD_MS);
+
+            gpio_set_level((gpio_num_t)MOTOR_IN1_PIN, 0);
+            gpio_set_level((gpio_num_t)MOTOR_IN2_PIN, 0);
+            gpio_set_level((gpio_num_t)MOTOR_IN3_PIN, 0);
+            gpio_set_level((gpio_num_t)MOTOR_IN4_PIN, 0);
+        } else {
+            gpio_set_level((gpio_num_t)MOTOR_IN1_PIN, 1);
+            gpio_set_level((gpio_num_t)MOTOR_IN2_PIN, 0);
+            gpio_set_level((gpio_num_t)MOTOR_IN3_PIN, 1);
+            gpio_set_level((gpio_num_t)MOTOR_IN4_PIN, 0);
+        }
+    }
+
+    void driveBackward(int duration) {
+        if (duration > 0) {
+            m_leftWheels = 2;
+            m_rightWheels = 2;
+
+            gpio_set_level((gpio_num_t)MOTOR_IN1_PIN, 0);
+            gpio_set_level((gpio_num_t)MOTOR_IN2_PIN, 1);
+            gpio_set_level((gpio_num_t)MOTOR_IN3_PIN, 0);
+            gpio_set_level((gpio_num_t)MOTOR_IN4_PIN, 1);
+
+            vTaskDelay(duration/portTICK_PERIOD_MS);
+
+            gpio_set_level((gpio_num_t)MOTOR_IN1_PIN, 0);
+            gpio_set_level((gpio_num_t)MOTOR_IN2_PIN, 0);
+            gpio_set_level((gpio_num_t)MOTOR_IN3_PIN, 0);
+            gpio_set_level((gpio_num_t)MOTOR_IN4_PIN, 0);
+        } else {
+            gpio_set_level((gpio_num_t)MOTOR_IN1_PIN, 0);
+            gpio_set_level((gpio_num_t)MOTOR_IN2_PIN, 1);
+            gpio_set_level((gpio_num_t)MOTOR_IN3_PIN, 0);
+            gpio_set_level((gpio_num_t)MOTOR_IN4_PIN, 1);            
+        }
+    }
+
+    void stop() {
+        m_leftWheels = 0;
+        m_rightWheels = 0;
+
+        gpio_set_level((gpio_num_t)MOTOR_IN1_PIN, 0);
+        gpio_set_level((gpio_num_t)MOTOR_IN2_PIN, 0);
+        gpio_set_level((gpio_num_t)MOTOR_IN3_PIN, 0);
+        gpio_set_level((gpio_num_t)MOTOR_IN4_PIN, 0);
+    }
+    //car1.turnLeft(angle/percentage) 0 == infinite turn
+    virtual void turnLeft(int turn) {
+        if (turn >= 0) {
+            m_leftWheels = 2;
+            m_rightWheels = 1;
+
+            gpio_set_level((gpio_num_t)MOTOR_IN1_PIN, 1);
+            gpio_set_level((gpio_num_t)MOTOR_IN2_PIN, 0);
+            gpio_set_level((gpio_num_t)MOTOR_IN3_PIN, 0);
+            gpio_set_level((gpio_num_t)MOTOR_IN4_PIN, 1);
+
+            vTaskDelay(turn/portTICK_PERIOD_MS);
+
+            gpio_set_level((gpio_num_t)MOTOR_IN1_PIN, 0);
+            gpio_set_level((gpio_num_t)MOTOR_IN2_PIN, 0);
+            gpio_set_level((gpio_num_t)MOTOR_IN3_PIN, 0);
+            gpio_set_level((gpio_num_t)MOTOR_IN4_PIN, 0);
+        }
+    }
+
+    virtual void turnRight(int turn) {
+        if (turn >= 0) {
+            m_leftWheels = 1;
+            m_rightWheels = 2;
+
+            gpio_set_level((gpio_num_t)MOTOR_IN1_PIN, 0);
+            gpio_set_level((gpio_num_t)MOTOR_IN2_PIN, 1);
+            gpio_set_level((gpio_num_t)MOTOR_IN3_PIN, 1);
+            gpio_set_level((gpio_num_t)MOTOR_IN4_PIN, 0);
+
+            vTaskDelay(turn/portTICK_PERIOD_MS);
+
+            gpio_set_level((gpio_num_t)MOTOR_IN1_PIN, 0);
+            gpio_set_level((gpio_num_t)MOTOR_IN2_PIN, 0);
+            gpio_set_level((gpio_num_t)MOTOR_IN3_PIN, 0);
+            gpio_set_level((gpio_num_t)MOTOR_IN4_PIN, 0);
+        }
     }
 };
--- components/SteeringLibrary/include/Vehicle4WDSteering.hpp
+++ components/SteeringLibrary/include/Vehicle4WDSteering.hpp
@@ -1,20 +1,35 @@
 #pragma once
 
-#include <iostream>
 #include "Vehicle4WD.hpp"
 
 class Vehicle4WDSteering : public Vehicle4WD {
 private:
-    int m_turnAngle;
+    int m_turnAngle;    // -40° - 40°
 
 public:
-    Vehicle4WDSteering(int direction, int turn) : Vehicle4WD(direction, direction), m_turnAngle(turn) {}
+    Vehicle4WDSteering(int direction, int speed, int turn = 0) : Vehicle4WD(direction, direction, speed), m_turnAngle(turn) {}
 
-    void turnLeft() override {
-        ;;
+    int getTurnAngle() const {
+        return m_turnAngle;
     }
 
-    void turnRight() override {
-        ;;
+    void turnLeft(int turn) override {
+        if (turn >= 0 && turn <= 40) {
+            m_turnAngle = -turn;
+
+            int duty = ((1.0+((90.0+m_turnAngle)/180.0))/20.0)*getPwmResolution();
+            ledc_set_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_1, duty);
+            ledc_update_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_1);
+        }
+    }
+
+    void turnRight(int turn) override {
+        if (turn >= 0 && turn <= 40) {
+            m_turnAngle = turn;
+
+            int duty = ((1.0+((90.0+m_turnAngle)/180.0))/20.0)*getPwmResolution();
+            ledc_set_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_1, duty);
+            ledc_update_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_1);
+        }
     }
 };
